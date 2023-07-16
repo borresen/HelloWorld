20c7b3cb39377d4517dff7d2ebfe5035a97e4135
Jan Børresen <jcb@vf.dk>
2023-07-16 13:32:14 +0200

    test 1

--- Program.cs
+++ Program.cs
@@ -6,8 +6,20 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Borresen");
+            // string fornavn = "Jan";
+            // string efternavn = "Børresen";
+            // Console.WriteLine("Hello World!");
+            // Console.WriteLine($"Mit navn er {fornavn} {efternavn}");
+            // Console.WriteLine($"Fornavn {fornavn} længde er {fornavn.Length}"); 
+            // Console.WriteLine($"Efternavn {efternavn} længde er {efternavn.Length}");
+            // fornavn = fornavn.Replace("Jan","Ulla");
+            // Console.WriteLine($"Mit navn er {fornavn} {efternavn}");
+            // Console.WriteLine(fornavn.ToUpper());
+            int a = 5;
+            int b = 15;
+            int c = a+b;
+            Console.WriteLine(c);
+                       
             Console.ReadLine();
         }
     }
